97a817ecb90e8719fe14bf3d61e285e4bf6618d6
music <music@coaadmins-MacBook-Pro.local>
2017-09-23 17:11:40 -0400

    fixed index error in myHann

--- myHann.m
+++ myHann.m
@@ -2,7 +2,7 @@ function [ w ] = myHann( block_size )
 
 w = zeros(block_size, 1);
 for n = 1:block_size
-    w(n) = (1 - cos((2 * pi * n)/(block_size - 1))) * .5;
+    w(n) = (1 - cos((2 * pi * (n - 1))/(block_size - 1))) * .5;
 end
 end
 
