6a25e3953ec24c2022ac75a75b70c04f5093b4df
Ryan Rose <rytrose@gmail.com>
2017-09-23 13:13:12 -0400

    functioning aggregateFeaturePerFile

--- aggregateFeaturesPerFile.m
+++ aggregateFeaturesPerFile.m
@@ -1,4 +1,4 @@
-function [fMean, fStd] = aggregateFeaturesPerFile(featureValues)
+function [aggFeatureMat] = aggregateFeaturesPerFile(featureMat)
 
 %% Aggregates the values in featureValues and returns the mean and standard
 % deviation, hence given 2 aggregated features per blocked feature per file.
@@ -10,3 +10,17 @@ function [fMean, fStd] = aggregateFeaturesPerFile(featureValues)
 %   fStd:   float number, standard deviation of features
 
 % Write your code below:
+aggFeatureMat = zeros(10, 1);
+fMat_size = size(featureMat);
+
+i = 1;
+while(i < fMat_size(1) + 1)
+   mean_i = (i*2) - 1;
+   std_i = i * 2;
+   aggFeatureMat(mean_i) = mean(featureMat(i, :));
+   aggFeatureMat(std_i) = std(featureMat(i, :));
+   
+   i = i + 1;
+end
+
+end
