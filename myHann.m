dc65da9fbf4e3c990011e9d59b99bb490a2cfc40
Ryan Rose <rytrose@gmail.com>
2017-09-23 11:29:48 -0400

    untested spectral centroid

---
+++ myHann.m
@@ -0,0 +1,8 @@
+function [ w ] = myHann( block_size )
+
+w = zeros(block_size, 1);
+for n = 1:block_size
+    w(n) = (1 - cos((2 * pi * n)/(block_size - 1))) * .5;
+end
+end
+
--- mySpectralCentroid.m
+++ mySpectralCentroid.m
@@ -11,4 +11,21 @@ function [specCentroid] = mySpectralCentroid(xb, fs)
 
 %% Write your code below:
 
+i = 1;
+size_xb = size(xb);
+numBlocks = size_xb(2);
+blockSize = size_xb(1);
+frequencies = ((fs/(2*blockSize))*[1:blockSize])';
+specCentroid = zeros(1, numBlocks);
+
+while(i < numBlocks)
+    window = xb(:,i).*myHann(blockSize);
+    mag = abs(fft(window, 2*blockSize));
+    mag = mag(1:blockSize);
+    specCentroid(i) = sum(frequencies.*mag)/sum(mag);
+    if(ISNAN(specCentroid(i)))
+        specCentroid(i) = 0.0;
+    end
+end
+
 end
