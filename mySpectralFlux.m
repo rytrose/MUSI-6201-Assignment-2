78aac8cf63f205f53f9282019923923574232f54
Ryan Rose <rytrose@gmail.com>
2017-09-23 12:30:41 -0400

    untested but functioning mySpectralFlux

--- mySpectralFlux.m
+++ mySpectralFlux.m
@@ -11,4 +11,29 @@ function [specFlux] = mySpectralFlux(xb)
 
 %% Write your code below:
 
+i = 1;
+size_xb = size(xb);
+numBlocks = size_xb(2);
+blockSize = size_xb(1);
+specFlux = zeros(1, numBlocks);
+
+while(i < numBlocks + 1)
+    window = xb(:,i).*myHann(blockSize);
+    mag = abs(fft(window, 2*blockSize));
+    mag = mag(1:blockSize);
+    
+    delta = zeros(blockSize, 1);
+    delta(1) = 0;
+    j = 2;
+    while(j < blockSize + 1)
+        delta(j) = mag(j) - mag(j - 1); 
+        j = j + 1;
+    end
+    specFlux(i) = sqrt(sum(delta.^2))/blockSize; 
+    if(isnan(specFlux(i)))
+        specFlux(i) = 0.0;
+    end
+    i = i + 1;
+end
+
 end
